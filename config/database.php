c53985a7b0172dbbb1aab9f25b42214250e78210
zyh19970909 <48995025+zyh19970909@users.noreply.github.com>
2019-07-02 23:17:48 +0800

    success

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'tp_test',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'mysql123456.',
     // 端口
     'hostport'        => '',
     // 连接dsn
